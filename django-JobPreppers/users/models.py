ea520de3083542009703d6644c1e36d05df2eba2
thewilliamlin <wlinn666@gmail.com>
2024-11-19 20:46:58 -0700

    updated app name to better reflect db data

---
+++ django-JobPreppers/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.TextField()
+    first_name = models.TextField()
+    last_name = models.TextField()
+    email = models.TextField()
