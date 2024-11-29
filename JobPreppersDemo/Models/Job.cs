c561bc1f88570fb9063a62b41d0d6bff96522800
Justin <justinellis15@outlook.com>
2024-11-29 13:03:27 -0700

    Updated .gitignore to track only necessary files and added project and silution folder

---
+++ JobPreppersDemo/Models/Job.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JobPreppersDemo.Models;
+
+public partial class Job
+{
+    public int jobID { get; set; }
+
+    public string title { get; set; } = null!;
+
+    public string location { get; set; } = null!;
+
+    public string pay_range { get; set; } = null!;
+
+    public string? description { get; set; }
+
+    public DateTime? postedAt { get; set; }
+
+    public DateTime fill_by_date { get; set; }
+
+    public string company { get; set; } = null!;
+}
