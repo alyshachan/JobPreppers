de75ee967e7480d64e30ed303411e9d43a5d37c5
Justin <justinellis15@outlook.com>
2024-12-02 22:37:58 -0700

    added User secrets for secruity

--- JobPreppersDemo/Contexts/ApplicationDbContext.cs
+++ JobPreppersDemo/Contexts/ApplicationDbContext.cs
@@ -25,16 +25,14 @@ public partial class ApplicationDbContext : DbContext
 
     public virtual DbSet<test> tests { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=jobprepperscapstone.cbgwos8q0ls4.us-east-2.rds.amazonaws.com;database=JobPreppersDB;port=3307;user id=JobPrepper;password=ILoveCanes2025!;sslmode=None", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.39-mysql"));
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
             .UseCollation("utf8mb4_0900_ai_ci")
             .HasCharSet("utf8mb4");
-
+        
         modelBuilder.Entity<Job>(entity =>
         {
             entity.HasKey(e => e.jobID).HasName("PRIMARY");
