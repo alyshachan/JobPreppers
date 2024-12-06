791dbe3abd2a2386d247e36101ba654b19c544a2
alysha <alyshachan2002@gmail.com>
2024-12-06 01:55:03 -0700

    Change Nav bar colors, update interviewer card pfp picture background

--- JobPreppers/src/Components/NavBar.js
+++ JobPreppers/src/Components/NavBar.js
@@ -39,8 +39,8 @@ function CustomLink({ to, children, className, ...props }) {
         className={classNames(
           className,
           isActive
-            ? "bg-gray-900 text-white"
-            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+            ? "bg-[#085630] text-white"
+            : "text-gray-300 hover:bg-[#0D7944] hover:text-white",
           "block rounded-md px-3 py-2 text-base font-medium"
         )}
       >
@@ -91,7 +91,7 @@ function NavBar({ firstName, lastName, profilePicture }) {
         <div className="relative flex h-24 items-center justify-between">
           {/* Mobile menu button */}
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-[#0D7944] hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
@@ -129,7 +129,7 @@ function NavBar({ firstName, lastName, profilePicture }) {
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button
               type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+              className="relative rounded-full bg-[#085630] p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
             >
               <span className="absolute -inset-1.5" />
               <span className="sr-only">View notifications</span>
