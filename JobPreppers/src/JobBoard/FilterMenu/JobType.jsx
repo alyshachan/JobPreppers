66a16b976ec4bfc58bb850ca2dc535f38b5f3f3b
tofu4U <122405386+tofu4U@users.noreply.github.com>
2024-12-06 02:39:47 -0700

    Type is working now

--- JobPreppers/src/JobBoard/FilterMenu/JobType.jsx
+++ JobPreppers/src/JobBoard/FilterMenu/JobType.jsx
@@ -1,24 +1,24 @@
-import { TextField, Autocomplete, Checkbox, Button } from "@mui/material";
-import React, { useState } from "react";
+import { TextField, Autocomplete, Checkbox } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import { Stack } from "@mui/material";
 import "./Menu.css";
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
 
-const JobType = ({ setFilters, jobs }) => {
-  // Memoizing the options list to avoid unnecessary re-renders
-  const List_Value = [
-    { id: 0, value: "Full-Time" },
-    { id: 1, value: "Part-Time" },
-    { id: 2, value: "Internship" },
-    { id: 3, value: "On-Campus" },
-    { id: 4, value: "Fellowship" },
-    { id: 5, value: "Graduate" },
-    { id: 6, value: "Volunteer" },
-  ];
+const List_Value = [
+  { id: 0, value: "Full-Time" },
+  { id: 1, value: "Part-Time" },
+  { id: 2, value: "Internship" },
+  { id: 3, value: "On-Campus" },
+  { id: 4, value: "Fellowship" },
+  { id: 5, value: "Graduate" },
+  { id: 6, value: "Volunteer" },
+]; 
 
+export default function JobType({ setFilters }) {
   const [selectValue, setSelectValue] = useState([]);
 
   const handleChange = (event, newValue) => {
@@ -28,40 +28,37 @@ const JobType = ({ setFilters, jobs }) => {
       ...prev,
       type: newValue.map((type) => type.value),
     }));
+
+    console.log(newValue);
   };
 
-  return (
-    <>
-      <div>
-        <Autocomplete
-          multiple
-          id="checkboxes-tags-demo"
-          className="drop-down"
-          options={List_Value}
-          limitTags={1}
-          disableCloseOnSelect
-          getOptionLabel={(option) => option.value}
-          value={selectValue}
-          onChange={handleChange}
-          renderOption={(props, option, { selected }) => {
-            const { key, ...optionProps } = props;
-            return (
-              <li key={key} {...optionProps}>
-                <Checkbox
-                  icon={icon}
-                  checkedIcon={checkedIcon}
-                  style={{ marginRight: 8 }}
-                  checked={selected}
-                />
-                {option.value}
-              </li>
-            );
-          }}
-          renderInput={(params) => <TextField {...params} label="Job Type" />}
-        />
-      </div>
-    </>
-  );
-};
 
-export default JobType;
+  return (
+    <Autocomplete
+      multiple
+      id="checkboxes-tags-demo"
+      options={List_Value}
+      className="drop-down"
+      disableCloseOnSelect
+      value={selectValue}
+      onChange={handleChange}
+      limitTags={1}
+      getOptionLabel={(option) => option.value}
+      renderOption={(props, option, { selected }) => {
+        const { key, ...optionProps } = props;
+        return (
+          <li key={key} {...optionProps}>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              style={{ marginRight: 8 }}
+              checked={selected}
+            />
+            {option.value}
+          </li>
+        );
+      }}
+      renderInput={(params) => <TextField {...params} label="Job Type" />}
+    />
+  );
+}
