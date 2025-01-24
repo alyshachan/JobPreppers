b757cd06814213246db90ce65e34d746cfeff0ca
alysha <alyshachan2002@gmail.com>
2025-01-24 15:27:06 -0700

    Profile displays db

--- frontend-jp/src/Pages/Profile.jsx
+++ frontend-jp/src/Pages/Profile.jsx
@@ -10,10 +10,49 @@ import styles from "../Components/Profile/Profile.module.css";
 
 function Profile({ edit = false }) {
   const { user, setAuthData } = useAuth(); // custom hook for authprovider
-  const { initialUser, setIntialUser } = useState(null); // !mt-[175px]
+  const { initialUser, setIntialUser } = useState(null);
+  const [educationDict, setEducationDict] = useState([]);
   const [skillsDict, setSkillsDict] = useState({});
+  const [experienceDict, setExperienceDict] = useState([]);
+  const [projectDict, setProjectDict] = useState([]);
+
 
   useEffect(() => {
+    const requestEducation = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/UserEducation/${user.userID}`,
+          {
+            credentials: "include", // include cookies
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("API Response: ", data); // Log the response to verify the structure
+
+          if (data) {
+            const newEducationDict = data.map((education) => ({
+              school_name: education.schoolName,
+              degree_name: education.degreeName,
+              study_name: education.studyName,
+              start_date: new Date(education.startDate),
+              end_date: new Date(education.endDate),
+              description: education.description, 
+            }));
+
+            setEducationDict((prevState) => {
+              if (JSON.stringify(prevState) !== JSON.stringify(newEducationDict)) {
+                return newEducationDict;
+              }
+              return prevState;
+            });
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
     const requestSkills = async () => {
       try {
         const response = await fetch(
@@ -50,9 +89,78 @@ function Profile({ edit = false }) {
         console.log(error);
       }
     };
+    const requestExperience = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/UserExperience/${user.userID}`,
+          {
+            credentials: "include", // include cookies
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("API Response: ", data); // Log the response to verify the structure
+
+          if (data) {
+            const newExperienceDict = data.map((experience) => ({
+              work_name: experience.workName,
+              location: experience.workLocation,
+              job_title: experience.jobTitle,
+              start_date: new Date(experience.startDate),
+              end_date: new Date(experience.endDate),
+              description: experience.description,
+            }));
+
+            setExperienceDict((prevState) => {
+              if (JSON.stringify(prevState) !== JSON.stringify(newExperienceDict)) {
+                return newExperienceDict;
+              }
+              return prevState;
+            });
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    const requestProjects = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/UserProject/${user.userID}`,
+          {
+            credentials: "include", // include cookies
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          console.log("API Response: ", data); // Log the response to verify the structure
+
+          if (data) {
+            const newProjectDict = data.map((project) => ({
+              project_title: project.projectTitle,
+              description: project.description, 
+            }));
+
+            setProjectDict((prevState) => {
+              if (JSON.stringify(prevState) !== JSON.stringify(newProjectDict)) {
+                return newProjectDict;
+              }
+              return prevState;
+            });
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
     if (user) {
+      requestEducation(); // populate education
       requestSkills(); // populate skills
+      requestExperience(); // populate experience
+      requestProjects(); // populate projects
       console.log("User: ", user);
     }
   }, [user]); // only populate when user exists
@@ -192,8 +300,8 @@ function Profile({ edit = false }) {
           </div>
 
           <div className={styles.pinnedInfo}>
-            {Object.keys(testEduDict).length > 0 && (
-              <EducationSection educationDict={testEduDict} edit={edit} />
+            {Object.keys(educationDict).length > 0 && (
+              <EducationSection educationDict={educationDict} edit={edit} />
             )}
             {Object.keys(skillsDict).length > 0 && (
               <SkillsSection skillsDict={skillsDict} edit={edit} />
@@ -201,12 +309,12 @@ function Profile({ edit = false }) {
           </div>
         </div>
 
-        {Object.keys(testExpDict).length > 0 && (
-          <ExperienceSection experienceDict={testExpDict} edit={edit} />
+        {Object.keys(experienceDict).length > 0 && (
+          <ExperienceSection experienceDict={experienceDict} edit={edit} />
         )}
 
-        {Object.keys(testProjDict).length > 0 && (
-          <ProjectSection projectDict={testProjDict} edit={edit} />
+        {Object.keys(projectDict).length > 0 && (
+          <ProjectSection projectDict={projectDict} edit={edit} />
         )}
 
         {edit && (
